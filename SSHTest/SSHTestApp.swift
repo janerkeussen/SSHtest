9a5fe61b2fc350aa09edddcb231d30f395f30d71
janerkeussen <zhanerke.usen@gmail.com>
2023-08-16 17:50:05 -0700

    Initial Commit

---
+++ SSHTest/SSHTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSHTestApp.swift
+//  SSHTest
+//
+//  Created by Zhanerke Ussen on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SSHTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
